51633e145ce2c6f428e7d839df53059aa8c2e817
WNA97 <wiweka.n.a@gmail.com>
2022-09-24 15:35:49 +0700

    Selesai mengerjakan halaman index.blade.php yang berisikan informasi data diri secara singkat seperti sebuah CV/resumé

---
+++ resources/views/index.blade.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Profile Tugas 1</title>
+        <link rel="icon" type="image/x-icon" href="/img/favicon.png" />
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigasi Halaman-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-image fixed-top" id="sideNav">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">Wiweka Naren Ananda</span>
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="/img/profile.jpg" alt="Wiweka Naren Ananda" /></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#certification">Certifications</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- Isi Halaman-->
+        <div class="container-fluid p-0">
+            <!-- Bagian About / Tengtang saya-->
+            <section class="resume-section" id="about">
+                <div class="resume-section-content">
+                    <h1 class="mb-0">
+                        Wiweka Naren
+                        <span class="text-danger">Ananda</span>
+                    </h1>
+                    <div class="subheading mb-5">
+                        Jl. Dr. Otten 15 · Pasirkaliki, Bandung 40171 · (+62)819-0750-7222 ·
+                        <a class="text-danger" href="mailto:wiweka.n.a@outlook.com">wiweka.n.a@outlook.com</a>
+                    </div>
+                    <p class="lead mb-5">I am experienced in IT Administration who is responsible for managing Linux servers, websites and local corporate networks. Perform optimization solutions on the website, especially SEO so that it can provide the best experience for companies in terms of digital marketing using websites.</p>
+                    <div class="social-icons">
+                        <a class="social-icon" href="https://www.linkedin.com/in/i-ngurah-gede-wiweka-naren-ananda-318b7a175/"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="social-icon" href="https://github.com/WNA97"><i class="fab fa-github"></i></a>
+                        <a class="social-icon" href="https://twitter.com/wiweka97"><i class="fab fa-twitter"></i></a>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Bagian Experience / Pengalaman saya sejauh ini-->
+            <section class="resume-section" id="experience">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Experience</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Information Technology Support Technician</h3>
+                            <div class="subheading mb-3">PT. Gapura Akuakultiva</div>
+                            <p>Perform optimization solutions on the website, especially SEO so that it can provide the best experience for companies in terms of digital marketing using websites.  I am also responsible for optimizing the company network to provide the best solution in pandemic conditions where employees have to work remotely.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-danger">January 2020 - Present</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Information Technology Support Technician (intern)</h3>
+                            <div class="subheading mb-3">PT. Gapura Liqua Solutions</div>
+                            <p>Build Web base Application for Asset Management, Optimize on site SEO and design content for website page of the company.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-danger">February 2019 - April 2019</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Network Technician</h3>
+                            <div class="subheading mb-3">CV. Puritaman IT Solutions</div>
+                            <p>Performing network installation, maintenance and upgrades in order to optimize computer networks using Mikrotik as a network management tool.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-danger">January 2016 - August 2016</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Bagian Education / Edukasi atau Studi yang pernah saya tempuh-->
+            <section class="resume-section" id="education">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Education</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">STMIK LIKMI Bandung</h3>
+                            <div class="subheading mb-3">Bachelor's degree</div>
+                            <div>Information systems - Information management path</div>
+                            <p>GPA: 3.60</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-danger">August 2021 - >August 2023</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Politeknik Pos Indonesia</h3>
+                            <div class="subheading mb-3">Diploma</div>
+                            <div>Information engineering</div>
+                            <p>GPA: 3.66</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-danger">September 2016 - September 2019</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Bagian Skills / Keterampilan yang saya miliki -->
+            <section class="resume-section" id="skills">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Skills</h2>
+                    <div class="subheading mb-3">Programming Languages</div>
+                    <ul class="list-inline dev-icons">
+                        <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-python"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
+                    </ul>
+                    <div class="subheading mb-3">Other Skills</div>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Network Management using <b>Mikrotik</b>
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Spreadsheet using <b>Microsoft Excel</b>
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Design Graphic using <b>Adobe Illustator, Corel Draw & Canva</b>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Bagian Interest/Hal yang membuat saya suka/tertarik-->
+            <section class="resume-section" id="interests">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Interests</h2>
+                    <p>Apart from studying Administrative IT, I am also interested in photography using smart phones, digital cameras and film cameras. Being in the IT field makes me interested in technology, especially in computer hardware which sometimes inspires me to implement it in companies. I like to spend time outside by riding a motorbike. I also like to listen to music while working on an office project or while reading to learn new things about technology.</p>
+                    <p class="mb-0">In my spare time, I prefer to spend time learning about the latest technology updates both hardware and software, making small website projects (usually to learn new things in website development) and micro controller projects using Arduino (usually to learn how electronics work and trying to implement some automation project in my house)</p>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Bagian Licenses & certifications/ Kursus atau Sertifikasi yang pernah saya ambil-->
+            <section class="resume-section" id="certification">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Licenses & certifications</h2>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fa-solid fa-chalkboard-user"></i></span>
+                            Data Science & Machine Learning Dengan Python - PT. Babastudio
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fa-solid fa-chalkboard-user"></i></span>
+                            Belajar SEO Dasar Untuk Pemula (Learn Basic SEO For Beginners) - Mau Belajar Apa
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fa-solid fa-chalkboard-user"></i></span>
+                            Belajar Mengenai Pentingnya Copywriting Dalam Sebuah Konten <br>(Learn about the Importance of Copywriting in Content) - Mau Belajar Apa
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fa-solid fa-chalkboard-user"></i></span>
+                            IC3 Computing Fundamentals Training - Global Standard 5 - Certiport & CV. Rekhindo Pratama
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
